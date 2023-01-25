a46cdbed27c07b7504a91f0c31c4071534aa5662
Syed Waleed Nawaz <syedwaleednawaz333@gmail.com>
2023-01-25 23:25:28 +0500

    add project on github

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
+   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
