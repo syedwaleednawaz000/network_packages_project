243d80c70a235545f083c828dbe63ab2f2ec27a6
Syed Waleed Nawaz <syedwaleednawaz333@gmail.com>
2023-01-29 00:16:56 +0500

    add new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 import 'package:network_packages_project/Screen/splashscreen.dart';
-import 'package:network_packages_project/Screen/test.dart';
 
-void main() {
+import 'package:path_provider/path_provider.dart' as pathProvider;
+
+Future<void> main() async {
+  Directory directory = await pathProvider.getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
-    return  ScreenUtilInit(
+    return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context , child) {
+      builder: (context, child) {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'NetWork Packages',
@@ -28,4 +34,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
